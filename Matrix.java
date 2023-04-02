c90abf0c875a0388bb56b66b6599606ee353a6c1
Diana Líšková <110021631+diafox@users.noreply.github.com>
2023-04-02 09:57:17 +0200

    oprava suborov

---
+++ Matrix.java
@@ -0,0 +1,10 @@
+import java.util.function.DoubleFunction;
+
+public interface Matrix {
+    int getRows();
+    int getColumns();
+    double getElement(int row, int column);
+    void setElement(int row, int column, double value);
+    Matrix map(DoubleFunction<Double> f);
+    Matrix createSameTypeMatrix();
+}
