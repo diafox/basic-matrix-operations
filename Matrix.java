b74ba75d4e0d41d0aad8ddf12c582764e8feef20
Diana Líšková <110021631+diafox@users.noreply.github.com>
2023-04-02 09:55:52 +0200

    Delete Matrix.java

--- Matrix.java
+++
@@ -1,9 +0,0 @@
-import java.util.function.DoubleFunction;
-
-public interface Matrix {
-    int getRows();
-    int getColumns();
-    double getElement(int row, int column);
-    void setElement(int row, int column, double value);
-    Matrix map(DoubleFunction<Double> f);
-}
